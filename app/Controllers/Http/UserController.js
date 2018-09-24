06cd5ced7cd505055b3cbe2cec4953cd42c127c8
arsalantezro <arsalan@tezrosolutions.com>
2018-09-24 18:43:10 +0500

    controllers setup

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -2,8 +2,7 @@
 const User = use('App/Models/User')
 
 class UserController {
-  
- 
+
     async signup({request, auth, response}) {
 
         const userData = request.only(['name', 'username', 'email', 'password'])
@@ -13,21 +12,38 @@ class UserController {
             //generete jwt token
             const token = await auth.generate(user)
 
-            return response.json({
+            return response.json({status: 'success', data: token})
+        } catch (error) {
 
-                status:'success',
-                data:token
-            })
-            } catch (error) {
-
-                return response.status(400).json({
-
-                    status:'error',
-                    message:'There was a problem creating the user, please try again later.'
-                })
-            }
+            return response
+                .status(400)
+                .json({status: 'error', message: 'There was a problem creating the user, please try again later.'})
         }
-
     }
 
-    module.exports = UserController
+    async login([request, auth, response]) {
+
+        try {
+            // validate the user credentials and generate a JWT token
+            const token = await auth.attempt(
+                request.input('email'),
+                reqest.input('password'))
+
+                return response.json({
+                    status:'success',
+                    data:token
+                })
+
+        } catch (error) {
+
+            response.status(400).json({
+
+                status:'error',
+                message:'Invalid email/password'
+            })
+        }
+    }
+
+}
+
+module.exports = UserController
